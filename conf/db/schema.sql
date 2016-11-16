3c928b85f59c5fd894201cd1e921a7164bf44a29
Ezequiel Surijon <surijon@gmail.com>
2016-11-16 14:39:14 -0300

    c

--- conf/db/schema.sql
+++ conf/db/schema.sql
@@ -84,7 +84,7 @@ CREATE TABLE aulatec.responsable(
 	pais char(2) NOT NULL,
 	es_docente boolean  NOT NULL DEFAULT false,
 	device_type varchar(8),
-	device_reg_id varchar(120),
+	device_reg_id varchar(1024),
 	CONSTRAINT responsable_id_pk PRIMARY KEY (id)
 );
 COMMENT ON TABLE aulatec.responsable
